b48f1710d50b49446f4b4b0251a10d89bae5945e
carlos henrique zacarias <carlos.zacarias21@hotmail.com>
2019-08-21 08:56:08 -0300

    cvnaosncoasn

--- CG2.1/CG2.1/Form1.Designer.cs
+++ CG2.1/CG2.1/Form1.Designer.cs
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbDDA = new System.Windows.Forms.RadioButton();
+            this.pbGraficos = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGraficos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rbDDA);
+            this.groupBox1.Location = new System.Drawing.Point(660, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 519);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Retas:";
+            // 
+            // rbDDA
+            // 
+            this.rbDDA.AutoSize = true;
+            this.rbDDA.Location = new System.Drawing.Point(66, 46);
+            this.rbDDA.Name = "rbDDA";
+            this.rbDDA.Size = new System.Drawing.Size(48, 17);
+            this.rbDDA.TabIndex = 0;
+            this.rbDDA.TabStop = true;
+            this.rbDDA.Text = "DDA";
+            this.rbDDA.UseVisualStyleBackColor = true;
+            // 
+            // pbGraficos
+            // 
+            this.pbGraficos.Image = ((System.Drawing.Image)(resources.GetObject("pbGraficos.Image")));
+            this.pbGraficos.InitialImage = null;
+            this.pbGraficos.Location = new System.Drawing.Point(12, 12);
+            this.pbGraficos.Name = "pbGraficos";
+            this.pbGraficos.Size = new System.Drawing.Size(642, 519);
+            this.pbGraficos.TabIndex = 1;
+            this.pbGraficos.TabStop = false;
+            this.pbGraficos.Click += new System.EventHandler(this.PbGraficos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 543);
+            this.Controls.Add(this.pbGraficos);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGraficos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbDDA;
+        private System.Windows.Forms.PictureBox pbGraficos;
     }
 }
 
--- CG2.1/CG2.1/Form1.cs
+++ CG2.1/CG2.1/Form1.cs
@@ -12,9 +12,50 @@ namespace CG2._1
 {
     public partial class Form1 : Form
     {
+        private Point cordenadas;
         public Form1()
         {
             InitializeComponent();
+            cordenadas = new Point(0, 0);
+        }
+
+        public void DDA(int x1, int y1, int x2, int y2)
+        {
+            int Length, I;
+            double X, Y, Xinc, Yinc;
+
+            Length = Math.Abs(x2 - x1);
+
+            if (Math.Abs(y2 - y1) > Length)
+                Length = Math.Abs(y2 - y1);
+            Xinc = (double)(x2 - x1) / Length;
+            Yinc = (double)(y2 - y1) / Length;
+
+            X = x1; Y = y1;
+            while (X < x2)
+            {
+                writePixel((int)Math.Round(X), (int)Math.Round(Y));
+                X = X + Xinc;
+                Y = Y + Yinc;
+            }
+        }
+
+        private void writePixel(int x, int y)
+        {
+            Bitmap m = ((Bitmap)(pbGraficos.Image));
+            m.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+            pbGraficos.Image = m;
+        }
+
+        private void PbGraficos_Click(object sender, EventArgs e)
+        {
+            if (cordenadas.X == 0 && cordenadas.Y == 0)
+                cordenadas = ((MouseEventArgs)e).Location;
+            else
+            {
+                if (rbDDA.Checked == true)
+                    DDA(cordenadas.X, cordenadas.Y, ((MouseEventArgs)e).Location.X, ((MouseEventArgs)e).Location.Y);
+            }
         }
     }
 }
